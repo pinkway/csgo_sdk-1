57f81735922a6120a16d996200a0c577987fef96
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-04-29 14:30:40 +0300

    Update i_debug_overlay.h

--- wok-csgo/sdk/interfaces/i_debug_overlay.h
+++ wok-csgo/sdk/interfaces/i_debug_overlay.h
@@ -2,6 +2,6 @@
 
 class i_debug_overlay {
 public:
-	VFUNC(add_capsule_overlay(const vec3_t& mins, const vec3_t& max, float& radius, col_t clr, float duration, bool ignorez), 23,
-		void(__thiscall*)(void*, const vec3_t&, const vec3_t&, float&, int, int, int, int, float, bool, bool), mins, max, radius, clr.r(), clr.g(), clr.b(), clr.a(), duration, 0, ignorez)
+	VFUNC(add_capsule_overlay(const vec3_t& mins, const vec3_t& maxs, float& radius, col_t clr, float duration, bool ignorez), 23,
+		void(__thiscall*)(void*, const vec3_t&, const vec3_t&, float&, int, int, int, int, float, bool, bool), mins, maxs, radius, clr.r(), clr.g(), clr.b(), clr.a(), duration, 0, ignorez)
 };
