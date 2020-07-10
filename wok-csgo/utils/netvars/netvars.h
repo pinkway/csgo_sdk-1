1ccf4bda9cd8d6ebd1352cd76c0c954d1875f3c3
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-07-10 18:21:33 +0300

    Update netvars.h

--- wok-csgo/utils/netvars/netvars.h
+++ wok-csgo/utils/netvars/netvars.h
@@ -14,66 +14,44 @@ namespace netvars {
 
 #define NETVAR(func, type, name) \
 	type& func { \
-		static const auto hash = fnv1a_rt(name); \
-		static const auto offset = netvars::get<uint32_t>(hash); \
+		static const auto offset = netvars::get<uint32_t>(fnv1a(name)); \
 		return *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset); \
 	}
 
 #define ANETVAR(func, type, size, name) \
 	std::array<type, size>& func { \
-		static const auto hash = fnv1a_rt(name); \
-		static const auto offset = netvars::get<uint32_t>(hash); \
+		static const auto offset = netvars::get<uint32_t>(fnv1a(name)); \
 		return *reinterpret_cast<std::array<type, size>*>(reinterpret_cast<uintptr_t>(this) + offset); \
 	}
 
 #define PNETVAR(func, type, name) \
 	type* func { \
-		static const auto hash = fnv1a_rt(name); \
-		static const auto offset = netvars::get<uint32_t>(hash); \
+		static const auto offset = netvars::get<uint32_t>(fnv1a(name)); \
 		return reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset); \
 	}
 
 #define NETVAR_OFFSET(func, type, name, add) \
 	type& func { \
-		static const auto hash = fnv1a_rt(name); \
-		static const auto offset = netvars::get<uint32_t>(hash); \
+		static const auto offset = netvars::get<uint32_t>(fnv1a(name)); \
 		return *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset + add); \
 	}
 
 #define MNETVAR(func, type, name, modifier) \
 	type& func { \
-		static const auto hash = fnv1a_rt(name); \
-		static const auto offset = netvars::get<uint32_t>(hash); \
+		static const auto offset = netvars::get<uint32_t>(fnv1a(name)); \
 		return **reinterpret_cast<type**>(reinterpret_cast<uintptr_t>(this) + offset * modifier); \
 	}
 
 #define NETPROP(func, name) \
 	static c_recv_prop* func { \
-		static const auto hash = fnv1a_rt(name); \
-		static const auto prop = netvars::get<c_recv_prop*>(hash); \
+		static const auto prop = netvars::get<c_recv_prop*>(fnv1a(name)); \
 		return prop; \
 	}
 
-#define OFFSET(func, type, offset) \
-	type& func { \
-		static const auto offset_ = offset; \
-		return *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset_); \
-	}
+#define OFFSET(func, type, offset) type& func { return *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset); }
 
-#define POFFSET(func, type, offset) \
-	type* func { \
-		static const auto offset_ = offset; \
-		return reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset_); \
-	}
+#define POFFSET(func, type, offset) type* func { return reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(this) + offset); }
 
-#define PPOFFSET(func, type, offset) \
-	type& func { \
-		static const auto offset_ = offset; \
-		return **reinterpret_cast<type**>(reinterpret_cast<uintptr_t>(this) + offset_); \
-	}
+#define PPOFFSET(func, type, offset) type& func { return **reinterpret_cast<type**>(reinterpret_cast<uintptr_t>(this) + offset); }
 
-#define MOFFSET(func, type, offset, modifier) \
-	type& func { \
-		static const auto offset_ = offset; \
-		return **reinterpret_cast<type**>(reinterpret_cast<uintptr_t>(this) + offset_ * modifier); \
-	}
+#define MOFFSET(func, type, offset, modifier) type& func { return **reinterpret_cast<type**>(reinterpret_cast<uintptr_t>(this) + offset * modifier); }
