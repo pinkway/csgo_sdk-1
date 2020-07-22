a96245d7a2aa77b196ab1606b25a45d1e4af6646
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-07-22 21:30:39 +0300

    Update fnv1a.h

--- wok-csgo/utils/obfuscation/fnv1a.h
+++ wok-csgo/utils/obfuscation/fnv1a.h
@@ -13,19 +13,19 @@ struct constant_holder_t {
 constexpr auto seed = 0x45C3370D;
 constexpr auto prime = 0x1000193;
 
-__forceinline uint32_t fnv1a_fl_rt(const char* key, uint32_t length) {
+__forceinline uint32_t fnv1a_fl_rt(const char* txt, uint32_t length) {
 	auto hash = seed;
 
 	for (int i = 0; i < length; i++) {
-		hash ^= key[i];
+		hash ^= txt[i];
 		hash *= prime;
 	}
 
 	return hash;
 }
 
-__forceinline uint32_t fnv1a_rt(const char* key) { return fnv1a_fl_rt(key, strlen(key)); }
+__forceinline uint32_t fnv1a_rt(const char* txt) { return fnv1a_fl_rt(txt, strlen(txt)); }
 
-constexpr uint32_t fnv1a_ct(const char* str, uint32_t value = seed) noexcept { return !*str ? value : fnv1a_ct(str + 1, static_cast<unsigned>(1ull * (value ^ static_cast<uint8_t>(*str)) * prime)); }
+constexpr uint32_t fnv1a_ct(const char* txt, uint32_t value = seed) noexcept { return !*txt ? value : fnv1a_ct(txt + 1, static_cast<unsigned>(1ull * (value ^ static_cast<uint8_t>(*txt)) * prime)); }
 
 #define fnv1a(txt) constant(fnv1a_ct(txt))
