10775ebf0aa348ee540c695e112b95f970df45be
sove <13748681+sowwe@users.noreply.github.com>
2020-08-04 23:29:43 +0300

    Update prediction.h

--- wok-csgo/features/ragebot/prediction.h
+++ wok-csgo/features/ragebot/prediction.h
@@ -14,32 +14,27 @@ private:
 	int* m_random_seed;
 
 	int post_think(c_cs_player* player) {
-		memory::get_vfunc<void(__thiscall*)(void*)>(interfaces::model_cache, 33)(interfaces::model_cache);
+		interfaces::model_cache->get_critical_section();
 
-		if (player->is_alive()
-			|| player->get<bool>(0x3A95)) {
-			memory::get_vfunc<void(__thiscall*)(void*)>(player, 339)(player);
+		if (player->is_alive() || player->is_ghost()) {
+			// Set correct collision bounds (may have changed in player movement code)
+			player->update_collistion_bounds();
 
-			if (player->get_flags() & FL_ONGROUND) {
+			if (player->get_flags() & FL_ONGROUND)
 				player->get_fall_velocity() = 0.f;
-			}
 
-			if (player->get_sequence() == -1) {
-				memory::get_vfunc<void(__thiscall*)(void*, int)>(player, 218)(player, 0);
-			}
+			// Don't allow bogus sequence on player
+			if (player->get_sequence() == -1)
+				player->set_sequence(0);
 
-			memory::get_vfunc<void(__thiscall*)(void*)>(player, 219)(player);
+			player->studio_frame_advance();
 
-			static const auto post_think_v_physics_fn = SIG("client.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB").cast<bool(__thiscall*)(void*)>();
-
-			post_think_v_physics_fn(player);
+			player->post_think_v_physics();
 		}
+		// Even if dead simulate entities
+		player->simulate_player_simulated_entities();
 
-		static const auto simulate_player_simulated_entities_fn = SIG("client.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72").cast<bool(__thiscall*)(void*)>();
-
-		simulate_player_simulated_entities_fn(player);
-
-		return memory::get_vfunc<int(__thiscall*)(void*)>(interfaces::model_cache, 34)(interfaces::model_cache);
+		return interfaces::model_cache->get_coarse_lock();
 	}
 public:
 	c_prediction() {
