0b15fca6f3642df14e8d5cf110fb05031bb4fc4b
sove <13748681+sowwe@users.noreply.github.com>
2020-08-27 15:38:12 +0300

    chams shadow fix 😵 (#14)
    * Update model_render.cpp
    
    * Update _structs.h

--- wok-csgo/hooks/vtables/model_render.cpp
+++ wok-csgo/hooks/vtables/model_render.cpp
@@ -3,6 +3,11 @@
 void __fastcall hooks::model_render::draw_model_execute::fn(i_model_render* ecx, void* edx, void* context, const draw_model_state_t& state, const model_render_info_t& info, matrix3x4_t* bones) {
 	static const auto original = m_model_render->get_original<T>(index);
 
+	if ((info.m_flags & STUDIO_SHADOW_DEPTH_TEXTURE) != 0) {
+		original(ecx, context, state, info, bones);
+		return;
+	}
+	
 	if (chams->on_draw_model(ecx, context, state, info, bones)) {
 		original(ecx, context, state, info, bones);
 	}
