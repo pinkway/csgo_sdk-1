06a124f38f2cdae243bf75c14db37dec8d288f12
sove <13748681+sowwe@users.noreply.github.com>
2020-10-02 17:43:53 +0300

    mathematica (#21)
    * Update math.cpp
    
    * Update math.h

--- wok-csgo/utils/math/math.h
+++ wok-csgo/utils/math/math.h
@@ -12,15 +12,117 @@ namespace math {
 
 	qangle_t calc_angle(const vec3_t& src, const vec3_t& dst);
 
-	float asin(float x);
+	/// <summary>
+	/// Fast inverse operation of sin of x.
+	/// </summary>
+	/// <param name="x">Value whose arc sine is computed</param>
+	/// <returns>Principal arc sine of x</returns>
+	double __forceinline __declspec (naked) __fastcall asin(double x) {
+		__asm {
+			fld		qword ptr [esp + 4]
+			fld		st
+			fmul		st, st
+			fld1
+			faddp		st(1), st
+			fsqrt
+			faddp		st(1), st
+			fldln2
+			fxch
+			fyl2x
+			ret		8
+		}
+	}
 
-	float atan2(float y, float x);
+	/// <summary>
+	/// Fast arc tangent of y/x.
+	/// </summary>
+	/// <param name="y">Value representing the proportion of the y-coordinate.</param>
+	/// <param name="x">Value representing the proportion of the x-coordinate.</param>
+	/// <returns>Principal value of the arc tangent of y/x.</returns>
+	double __forceinline __declspec (naked) __fastcall atan(double y, double x) {
+		__asm {
+			fld		qword ptr [esp + 4]
+			fld		qword ptr [esp + 12]
+			fpatan
+			ret		16
+		}
+	}
 
-	float atan(float x);
+	/// <summary>
+	/// Fast arc tangent of x.
+	/// </summary>
+	/// <param name="x">Value whose arc tangent is computed.</param>
+	/// <returns>Principal arc tangent of x.</returns>
+	double __forceinline __declspec (naked) __fastcall atan(double x) {
+		__asm {
+			fld		qword ptr [esp + 4] 
+			fld1 
+			fpatan 
+			ret		8 
+		}
+	}
 
-	float sin(float x);
+	/// <summary>
+	/// Fast sine of an angle of x radians.
+	/// </summary>
+	/// <param name="x">Value representing an angle expressed in radians.</param>
+	/// <returns>Sine of x radians.</returns>
+	double __forceinline __declspec (naked) __fastcall sin(double x) {
+		__asm {
+			fld		qword ptr [esp + 4]
+			fsin		
+			ret		8
+		}
+	}
 
-	float cos(float x);
+	/// <summary>
+	/// Fast cosine of an angle of x radians.
+	/// </summary>
+	/// <param name="x">Value representing an angle expressed in radians.</param>
+	/// <returns>Cosine of x radians.</returns>
+	double __forceinline __declspec (naked) __fastcall cos(double x) {
+		__asm {
+			fld		qword ptr [esp + 4]
+			fcos
+			ret		8
+		}
+	}
 
-	float acos(float x);
+	/// <summary>
+	/// Fast principal value of the arc cosine of x, expressed in radians.
+	/// </summary>
+	/// <param name="x">Value whose arc cosine is computed.</param>
+	/// <returns>Principal arc cosine of x.</returns>
+	double __forceinline __declspec (naked) __fastcall acos(double x) {
+		__asm {
+			fld		qword ptr [esp + 4] 
+			fld1 
+			fchs 
+			fcomp		st(1) 
+			fstsw		ax 
+			je			acos_po_pizde
+
+			fld		st(0) 
+			fld1 
+			fsubrp	st(1), st(0) 
+			fxch		st(1) 
+			fld1 
+			faddp		st(1), st(0) 
+			fdivp		st(1), st(0) 
+			fsqrt 
+			fld1 
+			jmp		acos_exit 
+
+			acos_po_pizde: 
+
+			fld1 
+			fldz 
+
+			acos_exit: 
+
+			fpatan 
+			fadd		st(0), st(0) 
+			ret		8 
+		}
+	}
 }
