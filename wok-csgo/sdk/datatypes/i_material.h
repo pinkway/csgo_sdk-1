4a6f7e33e6ecc05351b1dbfaf83cf6e99681427c
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-10-25 19:15:49 +0300

    #52

--- wok-csgo/sdk/datatypes/i_material.h
+++ wok-csgo/sdk/datatypes/i_material.h
@@ -8,5 +8,6 @@ public:
 	VFUNC(find_var(const char* name, bool* found, bool complain = true), 11, i_material_var*(__thiscall*)(void*, const char*, bool*, bool), name, found, complain)
 	VFUNC(alpha_modulate(float alpha), 27, void(__thiscall*)(void*, float), alpha)
 	VFUNC(color_modulate(float red, float green, float blue), 28, void(__thiscall*)(void*, float, float, float), red, green, blue)
+	VFUNC(set_flag(int flag, bool value), 29, void(__thiscall*)(void*, int, bool), flag, value)
 	VFUNC(is_error_material(), 42, bool(__thiscall*)(void*))
 };
