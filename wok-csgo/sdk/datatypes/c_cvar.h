cc31a897df5912a5cabd409d89ba2ea2c1fb974f
lagcomp <40600834+lagcomp@users.noreply.github.com>
2020-12-27 15:42:02 +0300

    ...

--- wok-csgo/sdk/datatypes/c_cvar.h
+++ wok-csgo/sdk/datatypes/c_cvar.h
@@ -1,6 +1,7 @@
 #pragma once
 
-using change_callback_t = void(__cdecl*)(void*, const char*, float);
+using change_callback0_t = void(__cdecl*)();
+using change_callback1_t = void(__cdecl*)(void*, const char*, float);
 
 class c_cvar {
 public:
@@ -10,46 +11,38 @@ public:
 	VFUNC(set_value(float value), 15, void(__thiscall*)(void*, float), value)
 	VFUNC(set_value(int value), 16, void(__thiscall*)(void*, int), value)
 
-	__forceinline char* get_default() { return m_default_value; }
-
-	__forceinline bool get_bool() { return get_int(); }
-
 	__forceinline float get_float() {
-		const auto xored_value = *reinterpret_cast<int*>(&m_float_value);
-		auto dexored_value = static_cast<int>(xored_value ^ reinterpret_cast<uintptr_t>(this));
-		return *reinterpret_cast<float*>(&dexored_value);
+		const auto xored = *reinterpret_cast<int*>(&m_float_value);
+		auto dexored = static_cast<int>(xored ^ reinterpret_cast<uintptr_t>(this));
+
+		return *reinterpret_cast<float*>(&dexored);
 	}
 
 	__forceinline int get_int() {
-		const auto xored_value = *reinterpret_cast<int*>(&m_int_value);
-		auto dexored_value = static_cast<int>(xored_value ^ reinterpret_cast<uintptr_t>(this));
-		return *reinterpret_cast<int*>(&dexored_value);
+		const auto xored = *reinterpret_cast<int*>(&m_int_value);
+		auto dexored = static_cast<int>(xored ^ reinterpret_cast<uintptr_t>(this));
+
+		return *reinterpret_cast<int*>(&dexored);
 	}
 
-	char							pad0[4];
-	c_cvar*							m_next;
-	int								m_registered;
-	char*							m_name;
-	char*							m_help_string;
-	bit_flag_t<uint32_t>			m_flags;
-	char							pad1[4];
-	c_cvar*							m_parent;
-	char*							m_default_value;
-	char*							m_string_value;
-	int								m_string_length;
-	float							m_float_value;
-	int								m_int_value;
-	int								m_has_min;
-	float							m_min_value;
-	int								m_has_max;
-	float							m_max_value;
-	c_utl_vector<change_callback_t>	m_callbacks;
-};
+	__forceinline bool get_bool() { return get_int(); }
 
-class c_cvar_iterator {
-public:
-	virtual void set_first() = 0;
-	virtual void next() = 0;
-	virtual bool is_valid() = 0;
-	virtual c_cvar* get() = 0;
+	char								pad0[4];
+	c_cvar*								m_next;
+	int									m_registered;
+	char*								m_name;
+	char*								m_help_string;
+	bit_flag_t<uint32_t>				m_flags;
+	change_callback0_t					m_callback;
+	c_cvar*								m_parent;
+	char*								m_default_value;
+	char*								m_string;
+	int									m_string_length;
+	float								m_float_value;
+	int									m_int_value;
+	int									m_has_min;
+	float								m_min_value;
+	int									m_has_max;
+	float								m_max_value;
+	c_utl_vector<change_callback1_t>	m_callbacks;
 };
--- wok-csgo/sdk/interfaces/i_cvar_system.h
+++ wok-csgo/sdk/interfaces/i_cvar_system.h
@@ -22,22 +22,21 @@ public:
 	virtual void			register_cmd(c_cvar* cvar) = 0;
 	virtual void			unregister_cmd(c_cvar* cvar) = 0;
 
-	VFUNC(get_cvar_iterator(), 42, c_cvar_iterator*(__thiscall*)(void*))
+	template <typename... A>
+	VFUNC(console_print(const col_t& clr, const char* txt, A... args), 25, void(__cdecl*)(void*, const col_t&, const char*, ...), clr, txt, args...)
 
 	__forceinline c_cvar* find_var(uint32_t hash) {
-		const auto it = get_cvar_iterator();
-
-		for (it->set_first(); it->is_valid(); it->next()) {
-			const auto cvar = it->get();
-			if (FNV1A_RT(cvar->m_name) != hash)
+		for (auto it = m_cmd_list_start; it; it = it->m_next) {
+			if (FNV1A_RT(it->m_name) != hash)
 				continue;
 
-			return cvar;
+			return it;
 		}
 
 		return nullptr;
 	}
 
-	template <typename... A>
-	VFUNC(console_print(const col_t& clr, const char* txt, A... args), 25, void(__cdecl*)(void*, const col_t&, const char*, ...), clr, txt, args...)
+	c_utl_vector<change_callback1_t>	m_global_change_callbacks;
+	char								pad0[28];
+	c_cvar*								m_cmd_list_start;
 };
